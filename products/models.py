f82f848f672d02a5dc245ce13512668defe886d7
Arif_Gafizov <agafizov@gmail.com>
2021-03-08 13:00:03 +0300

    Update product model

--- products/models.py
+++ products/models.py
@@ -1,19 +1,17 @@
 from django.db import models
-from django.utils.translation import ugettext as _
-
 
 from common.base_model import BaseModel
 
 
 class Product(BaseModel):
-    title = models.CharField(max_length=128)
-    description = models.TextField()
-    weight = models.IntegerField()
-    price = models.DecimalField(max_digits=13, decimal_places=2)
+    title = models.CharField('наименование', max_length=128)
+    description = models.TextField('описание')
+    weight = models.IntegerField('вес')
+    price = models.DecimalField('цена', max_digits=13, decimal_places=2)
 
     class Meta:
-        verbose_name = _('product')
-        verbose_name_plural = _('product')
+        verbose_name = 'товар'
+        verbose_name_plural = 'товары'
 
     def __str__(self):
         return self.title
