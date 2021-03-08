e60e01f3e5f77f877f4fa3f614f223c6ba690fdc
Arif_Gafizov <agafizov@gmail.com>
2021-03-08 13:00:14 +0300

    Update user model

--- users/models.py
+++ users/models.py
@@ -1,16 +1,15 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
-from django.utils.translation import ugettext as _
 
 
 class User(AbstractUser):
     middle_name = models.CharField('отчество', max_length=100)
-    phone_number = models.CharField(max_length=100)
-    address = models.CharField(max_length=100)
+    phone_number = models.CharField('номер телефона', max_length=100)
+    address = models.CharField('адрес', max_length=100)
 
     class Meta:
-        verbose_name = _('user')
-        verbose_name_plural = _('users')
+        verbose_name = 'пользователь'
+        verbose_name_plural = 'пользователи'
 
     def __str__(self):
         return self.last_name + ' ' + self.first_name
