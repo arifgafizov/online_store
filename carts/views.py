849c54e64fc634d3b4c791533e8bf23ca88fa207
Arif_Gafizov <agafizov@gmail.com>
2021-03-08 14:49:41 +0300

    Add CartViewSet

--- carts/views.py
+++ carts/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
 
-# Create your views here.
+from .models import Cart
+from .serializers import CartSerializer
+
+
+class CartViewSet((ModelViewSet)):
+    queryset = Cart.objects.all()
+    serializer_class = CartSerializer
+    #permission_classes = [IsAuthenticated]
+    #authentication_classes = (TokenAuthentication,)
+
+    def get_queryset(self):
+        return Cart.objects.filter(user=self.request.user)
