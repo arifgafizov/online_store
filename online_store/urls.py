8eae21919ee0d0dcff67d8b332347ff5eb2acdfb
Arif_Gafizov <agafizov@gmail.com>
2021-03-11 22:51:07 +0300

    Update urls.py

--- online_store/urls.py
+++ online_store/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from drf_yasg import openapi
-from rest_framework import permissions
+from rest_framework import permissions, authentication
 from drf_yasg.views import get_schema_view
 
 
@@ -24,9 +24,11 @@ schema_view = get_schema_view(
     openapi.Info(
         title='Online store DRF API',
         default_version='v1',
+
     ),
     public=True,
     permission_classes=(permissions.AllowAny,),
+    authentication_classes=(authentication.TokenAuthentication,),
 )
 
 api_urlpatterns = [
