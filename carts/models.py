e8ea4e48a6b9ab5f1b1fce7a43d787666eb3920d
Arif_Gafizov <agafizov@gmail.com>
2021-03-11 22:48:38 +0300

    Update carts app

--- carts/models.py
+++ carts/models.py
@@ -1,21 +1,30 @@
+from django.contrib.auth import get_user_model
 from django.db import models
-from django.conf import settings
 
 from products.models import Product
 
 
+user = get_user_model()
 class Cart(models.Model):
-    products = models.ManyToManyField(to=Product, through="CartProduct")
-    user = models.ForeignKey(settings.AUTH_USER_MODEL, related_name='carts', on_delete=models.CASCADE)
+    products = models.ManyToManyField(to=Product, through="CartProduct", help_text='товар')
+    user = models.OneToOneField(user, related_name='carts',
+                                on_delete=models.CASCADE,
+                                help_text='пользователь')
 
 
 class CartProduct(models.Model):
-    product = models.ForeignKey(Product, related_name='cart_products', on_delete=models.CASCADE)
-    cart = models.ForeignKey(Cart, related_name='cart_products', on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, related_name='cart_products',
+                                on_delete=models.CASCADE,
+                                help_text='товар в корзине')
+    cart = models.ForeignKey(Cart, related_name='cart_products',
+                            on_delete=models.CASCADE,
+                            help_text='корзина')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='дата создания')
+    meta_info = models.JSONField('мета информация', default=dict)
 
     class Meta:
         verbose_name = 'корзина'
         verbose_name_plural = 'корзины'
 
     def __str__(self):
-        return 'cart of ' + self.user.last_name
+        return 'cart of ' + self.cart_id
