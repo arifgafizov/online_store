167c5749c156d265e03f3ff7bb5ffc4df3f62dbd
Arif_Gafizov <agafizov@gmail.com>
2021-03-14 22:47:17 +0300

    Update CartAdmin

--- carts/admin.py
+++ carts/admin.py
@@ -5,3 +5,7 @@ from .models import Cart
 @admin.register(Cart)
 class CartAdmin(admin.ModelAdmin):
     list_display = ['id', 'user']
+
+    def get_queryset(self, request):
+        queryset = Cart.objects.select_related('user')
+        return queryset
