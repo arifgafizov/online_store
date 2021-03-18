202445609ab15bba38003cfe6fd1bf733dd6f17a
Arif_Gafizov <agafizov@gmail.com>
2021-03-18 21:23:59 +0300

    Update urls.py

--- carts/urls.py
+++ carts/urls.py
@@ -1,8 +1,8 @@
 from rest_framework.routers import DefaultRouter
 
-from .views import CartViewSet
-
+from .views import CartViewSet, AddProductViewSet
 
 router = DefaultRouter()
 router.register('carts', CartViewSet, basename='cart')
+router.register('add-products', AddProductViewSet, basename='add-product')
 urlpatterns = router.urls
