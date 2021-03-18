23bd56e521de287400f5fb01a6a2cebd4ba62d63
Arif_Gafizov <agafizov@gmail.com>
2021-03-18 21:24:49 +0300

    Rename related_name of user field of Cart model

--- carts/models.py
+++ carts/models.py
@@ -7,7 +7,7 @@ from products.models import Product
 user_model = get_user_model()
 class Cart(models.Model):
     products = models.ManyToManyField(to=Product, through="CartProduct", help_text='товар')
-    user = models.OneToOneField(user_model, related_name='carts',
+    user = models.OneToOneField(user_model, related_name='cart',
                                 on_delete=models.CASCADE,
                                 help_text='пользователь')
 
