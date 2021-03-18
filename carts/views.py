15d7c28726f1012017340b26ed90ebee9a59d7f9
Arif_Gafizov <agafizov@gmail.com>
2021-03-18 21:26:00 +0300

    Add AddProductViewSet

--- carts/views.py
+++ carts/views.py
@@ -1,14 +1,31 @@
-from rest_framework.viewsets import ModelViewSet
+from rest_framework.viewsets import ModelViewSet, mixins, GenericViewSet
 
-from .models import Cart
-from .serializers import CartSerializer
+from .models import Cart, CartProduct
+from .serializers import CartSerializer, AddProductSerializer
 
 
-class CartViewSet((ModelViewSet)):
+class CartViewSet(ModelViewSet):
     queryset = Cart.objects.all()
     serializer_class = CartSerializer
-    #permission_classes = [IsAuthenticated]
-    #authentication_classes = (TokenAuthentication,)
 
     def get_queryset(self):
         return Cart.objects.filter(user=self.request.user)
+
+
+class AddProductViewSet(mixins.CreateModelMixin,
+                         mixins.DestroyModelMixin,
+                         mixins.ListModelMixin,
+                         GenericViewSet):
+    queryset = CartProduct.objects.all()
+    serializer_class = AddProductSerializer
+
+    # def get_queryset(self):
+    #     return Cart.objects.filter(user=self.request.user)
+    #     # cart_obj = Cart.objects.filter(user=self.request.user)
+
+    # def get_serializer_class(self):
+    #     if self.request.method == 'GET':
+    #         return CartSerializer
+    #     else:
+    #         return AddProductSerializer
+
