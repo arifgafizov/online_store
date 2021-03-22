74e95580f73ca830c0905e0c090417b381b59928
Arif_Gafizov <agafizov@gmail.com>
2021-03-22 22:06:53 +0300

    Customizing User model in the admin panel

--- users/admin.py
+++ users/admin.py
@@ -1,5 +1,38 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from django.utils.translation import gettext, gettext_lazy as _
 
 from .models import User
 
-admin.site.register(User)
+
+@admin.register(User)
+class UserAdmin(UserAdmin):
+    fieldsets = (
+        (None, {'fields': ('username', 'password')}),
+        (_('Personal info'), {'fields': ('first_name',
+                                         'last_name',
+                                         'email',
+                                         'middle_name',
+                                         'phone_number',
+                                         'address')}),
+        (_('Permissions'), {
+            'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions'),
+        }),
+        (_('Important dates'), {'fields': ('last_login', 'date_joined')}),
+    )
+
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('username',
+                       'password1',
+                       'password2',
+                       'email',
+                       'first_name',
+                       'last_name',
+                       'middle_name',
+                       'phone_number',
+                       'address'
+                       ),
+        }),
+    )
