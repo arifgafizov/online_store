5615eea758ba8d736650e3030098ccfe32f59b36
Arif_Gafizov <agafizov@gmail.com>
2021-03-27 17:23:12 +0300

    Add CartProductSerializer, ProductInCartSerializer, CarWithPriceSerializer

--- carts/serializers.py
+++ carts/serializers.py
@@ -1,16 +1,8 @@
+from rest_framework import serializers
 from rest_framework.serializers import ModelSerializer
 
 from products.models import Product
-from products.serializers import ProductSerializer
 from .models import Cart, CartProduct
-
-
-class CartSerializer(ModelSerializer):
-    products = ProductSerializer(many=True)
-
-    class Meta:
-        model = Cart
-        fields = ['id', 'user', 'products']
 
 
 class AddProductSerializer(ModelSerializer):
@@ -24,3 +16,26 @@ class AddProductSerializer(ModelSerializer):
     def validate(self, attrs):
         attrs['cart'] = self.context['request'].user.cart
         return attrs
+
+
+class CartProductSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CartProduct
+        fields = ['id']
+
+
+class ProductInCartSerializer(serializers.ModelSerializer):
+    quantity = serializers.CharField()
+    positions = CartProductSerializer(source='cart_products', many=True)
+
+    class Meta:
+        model = Product
+        fields = ['id', 'title', 'quantity', 'price', 'positions']
+
+
+class CarWithPriceSerializer(serializers.ModelSerializer):
+    total_price = serializers.DecimalField(max_digits=11, decimal_places=2)
+
+    class Meta:
+        model = Cart
+        fields = ['total_price']
