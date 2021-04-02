8b6595c3b64bc8e44b93a404bee0ee460e41ebd5
Arif_Gafizov <agafizov@gmail.com>
2021-04-02 22:46:38 +0300

    Add urls.py to order app

---
+++ orders/urls.py
@@ -0,0 +1,8 @@
+from rest_framework.routers import DefaultRouter
+
+from .views import OrderViewSet
+
+
+router = DefaultRouter()
+router.register('orders', OrderViewSet, basename='order')
+urlpatterns = router.urls
