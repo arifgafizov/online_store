b654ccc6d7ff54a727a450df43eb0aa83c660199
Arif_Gafizov <agafizov@gmail.com>
2021-04-04 15:34:03 +0300

    Update Order model

--- orders/models.py
+++ orders/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 from carts.models import Cart
 from common.base_model import BaseModel
-from orders.data import choices
+from orders.orders_constants import ORDER_STATUSES, ORDER_STATUS_CREATED
 
 
 class Order(BaseModel):
@@ -10,9 +10,10 @@ class Order(BaseModel):
     delivery_at = models.DateTimeField(verbose_name='дата доставки')
     address = models.CharField('адрес доставки', max_length=200)
     phone = models.CharField('номер телефона', max_length=50)
-    status = models.CharField('статус заказа', max_length=100, choices=choices)
+    status = models.CharField('статус заказа', max_length=100, choices=ORDER_STATUSES, default=ORDER_STATUS_CREATED)
     products = models.JSONField("Продукты заказа", blank=True, default=dict)
     metadata = models.JSONField("Данные заказа", blank=True, default=dict)
+    total_price = models.DecimalField("Стоимость заказа", max_digits=11, decimal_places=2)
 
     class Meta:
         verbose_name = 'заказ'
