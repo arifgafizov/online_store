ace610eb2fce89013e3111ca7a15e03a570e5bb6
Arif_Gafizov <agafizov@gmail.com>
2021-04-11 22:55:43 +0300

    Add url 'pay-forms/'

--- orders/urls.py
+++ orders/urls.py
@@ -1,8 +1,11 @@
+from django.conf.urls import url
 from rest_framework.routers import DefaultRouter
 
-from .views import OrderViewSet
-
+from .views import OrderViewSet, TestPaymentView
 
 router = DefaultRouter()
 router.register('orders', OrderViewSet, basename='order')
 urlpatterns = router.urls
+views_urlpatterns = [
+  # pylint: disable=invalid-name
+  url(r'pay-forms/', TestPaymentView.as_view(), name='pay-forms')]
