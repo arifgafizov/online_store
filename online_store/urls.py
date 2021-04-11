3b5c59497f4ae00f8dce36cf3ab8f2784e42dea5
Arif_Gafizov <agafizov@gmail.com>
2021-04-11 22:48:54 +0300

    Add path 'payments/'

--- online_store/urls.py
+++ online_store/urls.py
@@ -19,6 +19,7 @@ from drf_yasg import openapi
 from rest_framework import permissions, authentication
 from drf_yasg.views import get_schema_view
 
+from orders.urls import views_urlpatterns
 
 schema_view = get_schema_view(
     openapi.Info(
@@ -42,4 +43,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/v1/', include(api_urlpatterns)),
     path('docs/', schema_view.with_ui('swagger', cache_timeout=0)),
+    path('payments/', include(views_urlpatterns)),
 ]
