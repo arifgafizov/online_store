04c4192b1db6532e157c6004ee67d083be2527be
Arif_Gafizov <agafizov@gmail.com>
2021-04-23 10:36:32 +0300

    Add settings.py STATIC_ROOT

--- online_store/settings.py
+++ online_store/settings.py
@@ -198,6 +198,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR/"static"
 
 MEDIA_ITEMS_IMAGE_DIR = 'products_images'
 
