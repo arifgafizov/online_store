37c434a69e1c906dee33709411cbacce55ddf317
Arif_Gafizov <agafizov@gmail.com>
2021-04-24 11:24:19 +0300

    Update ProductViewSet

--- products/views.py
+++ products/views.py
@@ -1,5 +1,6 @@
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.filters import OrderingFilter
+from rest_framework.permissions import AllowAny
 from rest_framework.viewsets import ModelViewSet
 
 from .filters import ProductFilter
