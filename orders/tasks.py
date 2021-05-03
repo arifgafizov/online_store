ae4083c38437e6189937851ffbd530f2272b3edc
Arif_Gafizov <agafizov@gmail.com>
2021-05-03 12:42:04 +0300

    Add task of send mail

---
+++ orders/tasks.py
@@ -0,0 +1,22 @@
+from celery import shared_task
+from django.core.mail import send_mail
+
+from orders.models import Order
+
+
+@shared_task
+def send_mail_order_status(order_id):
+    """
+       Отправка уведомления о статуса заказа по электронной почте.
+    """
+    order = Order.objects.get(id=order_id)
+    client_name = order.cart.user.first_name
+    client_email = order.cart.user.email
+    subject = f'Order nr. {order_id}'
+    message = f'Dear {client_name},\nStatus your order is {order.status}.'
+
+    mail_sent = send_mail(subject,
+                          message,
+                          'arif_test@rambler.ru',
+                          [client_email])
+    return mail_sent
