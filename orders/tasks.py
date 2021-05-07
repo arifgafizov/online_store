c52fe0d52b51f671d9dfece7936e0082a28a2664
Arif_Gafizov <agafizov@gmail.com>
2021-05-07 18:25:20 +0300

    Upgrade send_mail_order_status

--- orders/tasks.py
+++ orders/tasks.py
@@ -1,7 +1,7 @@
 from celery import shared_task
 
 from django.core.mail import send_mail
-from django.shortcuts import get_object_or_404
+from django.template.loader import render_to_string
 
 from orders.models import Order
 
@@ -11,14 +11,16 @@ def send_mail_order_status(order_id, order_status):
     """
        Отправка уведомления о статуса заказа по электронной почте.
     """
-    order = get_object_or_404(Order, pk=order_id)
+    order = Order.objects.get(pk=order_id)
     client_name = order.cart.user.first_name
     client_email = order.cart.user.email
     subject = f'Order nr. {order.id}'
-    message = f'Dear {client_name},\nStatus your order is {order_status}.'
+    path_to_template = './online_store/template/email.html'
+    msg_html = render_to_string(path_to_template, {'client_name': client_name, 'order_status': order_status})
 
     mail_sent = send_mail(subject,
-                          message,
-                          'arif_test@rambler.ru',
-                          [client_email])
+                          None,
+                          'tab-85@mail.ru',
+                          [client_email],
+                          html_message=msg_html)
     return mail_sent
