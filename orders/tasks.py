2b9c08d2d8aae2874f29aab779529fef65bde474
Arif_Gafizov <agafizov@gmail.com>
2021-05-09 22:43:49 +0300

    Update order tasks.py

--- orders/tasks.py
+++ orders/tasks.py
@@ -2,10 +2,12 @@ from celery import shared_task
 
 from django.core.mail import send_mail
 from django.template.loader import render_to_string
+from django.conf import settings
 
 from orders.models import Order
 
 
+PATH_TO_TEMPLATE = '/home/arif/PycharmProjects/online_store/online_store/template/email.html'
 @shared_task
 def send_mail_order_status(order_id, order_status):
     """
@@ -15,12 +17,11 @@ def send_mail_order_status(order_id, order_status):
     client_name = order.cart.user.first_name
     client_email = order.cart.user.email
     subject = f'Order nr. {order.id}'
-    path_to_template = './online_store/template/email.html'
-    msg_html = render_to_string(path_to_template, {'client_name': client_name, 'order_status': order_status})
+    msg_html = render_to_string(PATH_TO_TEMPLATE, {'client_name': client_name, 'order_status': order_status})
 
     mail_sent = send_mail(subject,
                           None,
-                          'tab-85@mail.ru',
+                          settings.EMAIL_HOST_USER,
                           [client_email],
                           html_message=msg_html)
     return mail_sent
