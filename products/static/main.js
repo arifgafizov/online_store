e6323202b7fb7d657b41df3d5f9dbba10cc77edb
Arif_Gafizov <agafizov@gmail.com>
2021-05-12 22:22:53 +0300

    Update main.js

--- products/static/main.js
+++ products/static/main.js
@@ -5,18 +5,37 @@ var app = new Vue({
         { text: 'Apple' },
         { text: 'Grape' },
         { text: 'Peach' }
-      ]
+      ],
+      username: '',
+      password: '',
+      isAuthenticated: false
+    },
+    methods: {
+        onSubmit: function (username, password) {
+            axios.post('/api/v1/users/auth/login/', {
+                'username': username,
+                'password': password,
+            }).then(function(response) {
+                console.log(response)
+            }).catch(function(error) {
+                console.log(error)
+                alert(error)
+            })
+        }
     },
     created() {
-     axios.get('/usersdata/user-data/')
-      .then(({data}) => {
-        console.debug(data)
-        this.checkAuthLoading = false
-      }).catch((err) => {
-        if (err.response?.status === 401) {
+     axios.get('/api/v1/users/auth/current/')
+       .then(({data}) => {
+          console.debug(data)
+          this.checkAuthLoading = false
+       }).catch((err) => {
+          if (err.response?.status === 401) {
 
-      } else {
-      }
-    })
-    }
+            this.isAuthenticated = true
+
+
+          } else {
+        }
+      })
+     }
 })
