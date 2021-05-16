6f320f169a6ecb24bbf1904c921c0b256ebe82dd
Arif_Gafizov <agafizov@gmail.com>
2021-05-16 17:54:24 +0300

    Update main.js

--- products/static/main.js
+++ products/static/main.js
@@ -2,9 +2,8 @@ var app = new Vue({
     el: '#app',
     data: {
       todos: [
-        { text: 'Apple' },
-        { text: 'Grape' },
-        { text: 'Peach' }
+        { text: 'Foo' },
+        { text: 'Bar' }
       ],
       username: '',
       password: '',
@@ -12,12 +11,15 @@ var app = new Vue({
     },
     methods: {
         onSubmit: function () {
+//          отправка пост запроса с данными username и password из формы
             axios.post('/api/v1/users/auth/login/', {
                 'username': this.username,
                 'password': this.password,
             }).then((response) => {
                 console.log(response)
+//              сохранение в переменной токена авторизации полученного из response data
                 const token = response.data.token
+//              добавление токена авторизации в localStorage с ключом AUTH_TOKEN
                 localStorage.setItem('AUTH_TOKEN', token)
                 this.isAuthenticated = true
             }).catch(function(error) {
@@ -27,7 +29,9 @@ var app = new Vue({
         }
     },
     created() {
+//    сохранение в переменной токена авторизации полученного из localStorage
      const token = localStorage.getItem('AUTH_TOKEN')
+//     отправка гет запроса в заголовке которого токен авторизации
      axios.get('/api/v1/users/auth/current/', {
         headers: {
           Authorization: "Token " + token
