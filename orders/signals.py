21918b51cb3d540df58480c454fe21c77ba1af6d
Arif_Gafizov <agafizov@gmail.com>
2021-05-16 11:31:37 +0300

    Update signals.py

--- orders/signals.py
+++ orders/signals.py
@@ -2,15 +2,22 @@ from django.db.models import signals
 from django.dispatch import receiver
 
 from orders.models import Order
-from orders.tasks import send_mail_order_status
+from orders.tasks import send_mail_order_status, send_mail_create_order
 
 
 @receiver(signal=signals.pre_save, sender=Order)
 def pre_save_status(instance, **kwargs):
-    original_status = Order.objects.get(pk=instance.pk).status
-    instance.context['original_status'] = original_status
+    try:
+        original_status = Order.objects.get(pk=instance.pk).status
+    except Order.DoesNotExist:
+        pass
+    else:
+        instance.context['original_status'] = original_status
 
 @receiver(signal=signals.post_save, sender=Order)
 def post_save_status(instance, **kwargs):
-    if not instance.status == instance.context['original_status']:
-        send_mail_order_status.delay(instance.id, instance.status)
+    if kwargs.get('created'):
+        send_mail_create_order(instance.id)
+    else:
+        if not instance.status == instance.context['original_status']:
+            send_mail_order_status.delay(instance.id, instance.status)
