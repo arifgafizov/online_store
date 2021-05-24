b0eb2d6592b3012f2098c8ef411a5c09ad68cb81
Arif_Gafizov <agafizov@gmail.com>
2021-05-24 20:51:58 +0300

    Add product_detail.js

---
+++ products/static/product_detail.js
@@ -0,0 +1,47 @@
+var app = new Vue({
+    el: '#appProductDetail',
+    data: {
+      title: '',
+      description: '',
+      price: '',
+      weight: '',
+      isActive: false
+    },
+    methods: {
+      addToCart: function (product_id) {
+//          сохранение в переменной токена авторизации полученного из localStorage
+            const token = localStorage.getItem('AUTH_TOKEN')
+//          отправка пост запроса с данными username и password из формы
+          axios.post('/api/v1/cart-products/', {
+                headers: {
+                    Authorization: "Token " + token
+                },
+                'product': product_id
+            }).then((response) => {
+
+            console.log(response)
+
+          }).catch(function(error) {
+                console.log(error)
+                alert(error)
+            })
+      }
+    },
+    created() {
+          const id = location.pathname.split('/').pop()
+          axios.get('/api/v1/products/' + id
+            ).then(response => {
+            console.log(response)
+//          добавление в products списка товаров полученного из response data results
+            this.title = response.data.title
+            this.description = response.data.description
+            this.price = response.data.price
+            this.weight = response.data.weight || 'вес не определен'
+//            if (response.data.weight) {
+//                    this.weight = response.data.weight
+//                } else {
+//                    this.weight = 'вес не определен'
+//                }
+          })
+     }
+})
