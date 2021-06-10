bed4d1b8e444b111bc79580348ba4cdecb87cadc
Arif_Gafizov <agafizov@gmail.com>
2021-06-10 17:25:32 +0300

    Update UserSerializer

--- users/serializers.py
+++ users/serializers.py
@@ -10,6 +10,7 @@ class UserSerializer(ModelSerializer):
         fields = [
             'id',
             'username',
+            'password',
             'email',
             'first_name',
             'last_name',
