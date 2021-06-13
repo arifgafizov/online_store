a153f4cd16a8e8f930d051a7311532c624462b6b
Arif_Gafizov <agafizov@gmail.com>
2021-06-13 22:50:04 +0300

    Update User model

--- users/models.py
+++ users/models.py
@@ -3,9 +3,9 @@ from django.db import models
 
 
 class User(AbstractUser):
-    middle_name = models.CharField('отчество', max_length=100)
-    phone_number = models.CharField('номер телефона', max_length=100)
-    address = models.CharField('адрес', max_length=100)
+    middle_name = models.CharField('отчество', max_length=100, blank=True, null=True)
+    phone_number = models.CharField('номер телефона', max_length=100, blank=True, null=True)
+    address = models.CharField('адрес', max_length=100, blank=True, null=True)
 
     class Meta:
         verbose_name = 'пользователь'
