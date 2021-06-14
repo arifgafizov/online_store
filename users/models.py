ea2a94f98d2f40b3a85715f78ff34ac90883f744
Arif_Gafizov <agafizov@gmail.com>
2021-06-14 11:15:08 +0300

    Add PreUser model

--- users/models.py
+++ users/models.py
@@ -3,9 +3,9 @@ from django.db import models
 
 
 class User(AbstractUser):
-    middle_name = models.CharField('отчество', max_length=100, blank=True, null=True)
-    phone_number = models.CharField('номер телефона', max_length=100, blank=True, null=True)
-    address = models.CharField('адрес', max_length=100, blank=True, null=True)
+    middle_name = models.CharField('отчество', max_length=100, blank=True)
+    phone_number = models.CharField('номер телефона', max_length=100, blank=True)
+    address = models.CharField('адрес', max_length=100, blank=True)
 
     class Meta:
         verbose_name = 'пользователь'
@@ -13,3 +13,21 @@ class User(AbstractUser):
 
     def __str__(self):
         return str(self.id) + ' ' + self.username
+
+
+class PreUser(models.Model):
+    username = models.CharField(
+        'логин',
+        max_length=150,
+        unique=True,
+        error_messages={
+            'unique': "A user with that username already exists."
+        })
+    password = models.CharField('пароль', max_length=128)
+    uuid_token = models.CharField('токен', max_length=150)
+    first_name = models.CharField('имя', max_length=150, blank=True)
+    last_name = models.CharField('фамилия', max_length=150, blank=True)
+    email = models.EmailField('ел. адрес', blank=True)
+    middle_name = models.CharField('отчество', max_length=100, blank=True)
+    phone_number = models.CharField('номер телефона', max_length=100, blank=True)
+    address = models.CharField('адрес', max_length=100, blank=True)
