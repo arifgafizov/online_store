95d01c41b56039c37826975844caa529a0633411
Arif_Gafizov <agafizov@gmail.com>
2021-06-14 16:10:23 +0300

    Add PreUserSerializer

--- users/serializers.py
+++ users/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework.serializers import ModelSerializer
 
-from .models import User
+from .models import User, PreUser
 
 
 class UserSerializer(ModelSerializer):
@@ -11,6 +11,24 @@ class UserSerializer(ModelSerializer):
             'id',
             'username',
             'password',
+            'email',
+            'first_name',
+            'last_name',
+            'middle_name',
+            'phone_number',
+            'address',
+        ]
+
+
+class PreUserSerializer(ModelSerializer):
+
+    class Meta:
+        model = PreUser
+        fields = [
+            'id',
+            'username',
+            'password',
+            'uuid_token',
             'email',
             'first_name',
             'last_name',
