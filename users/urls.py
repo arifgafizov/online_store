e559b402284fedfad85cbb0ca89deff3916dcd92
Arif_Gafizov <agafizov@gmail.com>
2021-06-14 16:12:56 +0300

    Update urls.py of users app

--- users/urls.py
+++ users/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
 from rest_framework.authtoken.views import obtain_auth_token
 
-from users.views import RegisterUserView, CurrentUserRetrieveUpdateView
+from users.views import RegisterPreUserView, CurrentUserRetrieveUpdateView
 
 urlpatterns = [
     path('auth/login/',  obtain_auth_token),
-    path('auth/register/', RegisterUserView.as_view(), name='register'),
+    path('auth/register/', RegisterPreUserView.as_view(), name='register'),
     path('auth/current/', CurrentUserRetrieveUpdateView.as_view(), name='current'),
 ]
