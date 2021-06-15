3ba1c9e5b0a5eb0e3294b93b1ec05d8f00b6c617
Arif_Gafizov <agafizov@gmail.com>
2021-06-15 23:45:47 +0300

    Add urls.py path 'auth/register-complete/'

--- users/urls.py
+++ users/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 from rest_framework.authtoken.views import obtain_auth_token
 
-from users.views import RegisterPreUserView, CurrentUserRetrieveUpdateView
+from users.views import RegisterPreUserView, CurrentUserRetrieveUpdateView, RegisterUserView
 
 urlpatterns = [
     path('auth/login/',  obtain_auth_token),
     path('auth/register/', RegisterPreUserView.as_view(), name='register'),
+    path('auth/register-complete/', RegisterUserView.as_view(), name='register-complete'),
     path('auth/current/', CurrentUserRetrieveUpdateView.as_view(), name='current'),
 ]
