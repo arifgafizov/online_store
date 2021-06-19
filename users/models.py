498dca001296ad4cae054f3fcc40afd2e585f4e9
Arif_Gafizov <agafizov@gmail.com>
2021-06-19 16:43:27 +0300

    Update PreUser model

--- users/models.py
+++ users/models.py
@@ -16,18 +16,16 @@ class User(AbstractUser):
 
 
 class PreUser(models.Model):
-    username = models.CharField(
-        'логин',
-        max_length=150,
-        unique=True,
-        error_messages={
-            'unique': "A user with that username already exists."
-        })
-    password = models.CharField('пароль', max_length=128)
+    username = models.CharField('логин', max_length=150)
     uuid_token = models.CharField('токен', max_length=150, blank=True)
     first_name = models.CharField('имя', max_length=150, blank=True)
     last_name = models.CharField('фамилия', max_length=150, blank=True)
-    email = models.EmailField('ел. адрес', blank=True)
+    email = models.EmailField('ел. адрес', max_length=150)
     middle_name = models.CharField('отчество', max_length=100, blank=True)
     phone_number = models.CharField('номер телефона', max_length=100, blank=True)
     address = models.CharField('адрес', max_length=100, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='дата создания')
+
+    class Meta:
+        verbose_name = 'предварительный пользователь'
+        verbose_name_plural = 'предварительные пользователи'
