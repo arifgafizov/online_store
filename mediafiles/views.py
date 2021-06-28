326a624322bae84b903daad7a33c34367f0ec93d
Arif_Gafizov <agafizov@gmail.com>
2021-06-28 11:07:35 +0300

    Update FileUploadView

--- mediafiles/views.py
+++ mediafiles/views.py
@@ -1,4 +1,5 @@
 import os
+from datetime import datetime
 
 from rest_framework import views, status
 from rest_framework.parsers import MultiPartParser
@@ -12,17 +13,19 @@ class FileUploadView(views.APIView):
     parser_classes = [MultiPartParser]
 
 
-    def post(self, request, domain, format='jpg'):
+    def post(self, request, domain):
+
         def save_file(path, f):
             with open(path, 'wb+') as destination:
                 for chunk in f.chunks():
                     destination.write(chunk)
 
         up_file = request.data['file']
-        dir = os.path.join(settings.MEDIA_ROOT, domain)
-        os.makedirs(dir, exist_ok=True)
-        path = os.path.join(dir, str(up_file))
+        now = datetime.now().isoformat()
+        media_dir = os.path.join(settings.MEDIA_ROOT, domain, now)
+        os.makedirs(media_dir, exist_ok=True)
+        path_to_file = os.path.join(media_dir, str(up_file))
 
-        save_file(path, up_file)
+        save_file(path_to_file, up_file)
 
         return Response(status.HTTP_201_CREATED)
