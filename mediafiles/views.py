cc827a874609dd1c4c94a2dd088afa2b83c05a34
Arif_Gafizov <agafizov@gmail.com>
2021-06-28 10:43:41 +0300

    Update FileUploadView

--- mediafiles/views.py
+++ mediafiles/views.py
@@ -1,15 +1,28 @@
-from django.shortcuts import render
+import os
+
 from rest_framework import views, status
-from rest_framework.parsers import FileUploadParser
+from rest_framework.parsers import MultiPartParser
+from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
+from django.conf import settings
 
 
 class FileUploadView(views.APIView):
-    parser_classes = [FileUploadParser]
+    permission_classes = [AllowAny]
+    parser_classes = [MultiPartParser]
 
-    def post(self, request, filename, format=None):
-        file_obj = request.data['file']
-        # ...
-        # do some stuff with uploaded file
-        # ...
-        return Response(status=status.HTTP_200_OK)
+
+    def post(self, request, domain, format='jpg'):
+        def save_file(path, f):
+            with open(path, 'wb+') as destination:
+                for chunk in f.chunks():
+                    destination.write(chunk)
+
+        up_file = request.data['file']
+        dir = os.path.join(settings.MEDIA_ROOT, domain)
+        os.makedirs(dir, exist_ok=True)
+        path = os.path.join(dir, str(up_file))
+
+        save_file(path, up_file)
+
+        return Response(status.HTTP_201_CREATED)
