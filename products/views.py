17ca02c0c766afae05bb36c8339b4f84d9999a2d
Arif_Gafizov <agafizov@gmail.com>
2021-06-29 11:12:17 +0300

    Add AddProductView

--- products/views.py
+++ products/views.py
@@ -1,13 +1,14 @@
 from django.views.generic import TemplateView
 from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework import mixins
 from rest_framework.filters import OrderingFilter
 from rest_framework.permissions import AllowAny
-from rest_framework.viewsets import ModelViewSet
+from rest_framework.viewsets import ModelViewSet, GenericViewSet
 
 from .filters import ProductFilter
 from .models import Product
 from .paginations import ProductPageNumberPagination
-from .serializers import ProductSerializer
+from .serializers import ProductSerializer, AddProductSerializer
 
 
 class ProductViewSet(ModelViewSet):
@@ -20,11 +21,19 @@ class ProductViewSet(ModelViewSet):
     filterset_fields = ['price']
     ordering = ['price']
 
+
+class AddProductView(mixins.CreateModelMixin, GenericViewSet):
+    queryset = Product.objects.all()
+    serializer_class = AddProductSerializer
+
+
 class IndexView(TemplateView):
     template_name = 'index.html'
 
+
 class ProductsView(TemplateView):
     template_name = 'products.html'
+
 
 class ProductDetailView(TemplateView):
     template_name = 'product_detail.html'
