ed821015cd5d578b77b3043aafb182af94b4c867
Arif_Gafizov <agafizov@gmail.com>
2021-06-29 22:56:06 +0300

    Add path 'add-products'

--- products/urls.py
+++ products/urls.py
@@ -1,8 +1,13 @@
+from django.urls import path
 from rest_framework.routers import DefaultRouter
 
-from .views import ProductViewSet
+from .views import ProductViewSet, AddProductView
 
 
 router = DefaultRouter()
 router.register('products', ProductViewSet, basename='product')
-urlpatterns = router.urls
+urlpatterns = [
+    path('add-products', AddProductView.as_view({'post': 'create'}), name='add-products')
+
+]
+urlpatterns += router.urls
