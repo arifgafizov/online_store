823d33b96b4e5a5fabfe71083e7360cf956f7e8d
Arif_Gafizov <agafizov@gmail.com>
2021-07-01 22:33:29 +0300

    Update ProductViewSet

--- products/views.py
+++ products/views.py
@@ -3,7 +3,7 @@ from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import mixins
 from rest_framework.filters import OrderingFilter
 from rest_framework.permissions import AllowAny
-from rest_framework.viewsets import ModelViewSet, GenericViewSet
+from rest_framework.viewsets import ModelViewSet, GenericViewSet, ReadOnlyModelViewSet
 
 from .filters import ProductFilter
 from .models import Product
@@ -11,7 +11,7 @@ from .paginations import ProductPageNumberPagination
 from .serializers import ProductSerializer, AddNewProductSerializer
 
 
-class ProductViewSet(ModelViewSet):
+class ProductViewSet(ReadOnlyModelViewSet):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
     pagination_class = ProductPageNumberPagination
