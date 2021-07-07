0d7483bb4213345dabfd8669417fed8cd3cd066d
Arif_Gafizov <agafizov@gmail.com>
2021-07-07 23:35:18 +0300

    Add media constrains

---
+++ mediafiles/media_constants.py
@@ -0,0 +1,5 @@
+ACCEPTABLE_VALUES = {
+    'image': {'extension': ['jpg', 'jpeg', 'png', 'tif', 'tiff', 'gif'], 'size': 30.0*1024**2},
+    'video': {'extension': ['avi', 'mp4', 'mpeg', 'mod', '3gp', 'mkv'], 'size': 200.0*1024**2},
+    'document': {'extension': ['pdf', 'doc', 'docx', 'txt'], 'size': 30.0*1024**2}
+}
