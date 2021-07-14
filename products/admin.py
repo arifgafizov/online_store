852aeac6b1435121c53a7a8d1689f22827f0660a
Arif_Gafizov <agafizov@gmail.com>
2021-07-14 15:25:18 +0300

    Add ProductAdmin

--- products/admin.py
+++ products/admin.py
@@ -2,4 +2,7 @@ from django.contrib import admin
 
 from .models import Product
 
-admin.site.register(Product)
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['id', 'title', 'price', 'is_deleted']
