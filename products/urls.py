8ce656c401ef7f5377e923d5b27c6877e17371e6
Arif_Gafizov <agafizov@gmail.com>
2021-07-18 15:52:23 +0300

    Update urls.py of products app

--- products/urls.py
+++ products/urls.py
@@ -1,13 +1,9 @@
-from django.urls import path
 from rest_framework.routers import DefaultRouter
 
-from .views import ProductViewSet, AddProductView
+from .views import ProductViewSet, CRUDProductView
 
 
 router = DefaultRouter()
 router.register('products', ProductViewSet, basename='product')
-urlpatterns = [
-    path('add-products', AddProductView.as_view({'post': 'create'}), name='add-products')
-
-]
-urlpatterns += router.urls
+router.register('crud-products', CRUDProductView, basename='crud-products')
+urlpatterns = router.urls
